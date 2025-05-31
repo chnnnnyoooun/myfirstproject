669e8e8c474d75c524f1f9713fda8f3ae579d112
chnnnnyoooun <limchaeyum11@gmail.com>
2025-05-31 13:04:52 +0900

    Update 01_오목게임.py

--- pages/01_오목게임.py
+++ pages/01_오목게임.py
@@ -3,11 +3,17 @@ import streamlit as st
 st.set_page_config(page_title="오목 게임", layout="wide")
 st.title("🎮 오목 게임 (돌 크기 키우기 & 판 작게)")
 
-BOARD_SIZE = 10  # 10x10 판으로 축소
+BOARD_SIZE = 10  # 10x10 판
 
 if "board" not in st.session_state:
     st.session_state.board = [["" for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
     st.session_state.turn = "●"
+
+def place_stone(i, j):
+    if st.session_state.board[i][j] == "":
+        st.session_state.board[i][j] = st.session_state.turn
+        st.session_state.turn = "○" if st.session_state.turn == "●" else "●"
+        st.experimental_rerun()
 
 for i in range(BOARD_SIZE):
     cols = st.columns(BOARD_SIZE)
@@ -15,12 +21,9 @@ for i in range(BOARD_SIZE):
         cell = st.session_state.board[i][j]
         if cell == "":
             if cols[j].button(" ", key=f"{i}-{j}"):
-                st.session_state.board[i][j] = st.session_state.turn
-                st.session_state.turn = "○" if st.session_state.turn == "●" else "●"
-                st.experimental_rerun()
+                place_stone(i, j)
         else:
-            # 돌 글자 크기 키우기 (HTML + 마크다운)
-            stone_html = f"<p style='font-size: 32px; text-align:center;'>{cell}</p>"
+            stone_html = f"<p style='font-size: 32px; text-align:center; margin:0'>{cell}</p>"
             cols[j].markdown(stone_html, unsafe_allow_html=True)
 
 st.markdown("---")
